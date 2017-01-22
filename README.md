991f5030354597bfe1ee7f77171573c3da1ac27d
Harsh Narang <harshn@Harshs-MacBook-Pro.local>
2017-01-22 23:45:25 +0530

    add README.md change assigment2/script2.sh stuff

---
+++ README.md
@@ -0,0 +1,2 @@
+My solutions to the various assignments of CS251
+Please do not copy :P
--- assignment2/script2.sh
+++ assignment2/script2.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 declare -a myTestArray
+myTestArray=(1 2 3 4)
+echo ${myTestArray[*]}
+unset myTestArray[1]
+echo ${myTestArray[1]}
